5bdfe116b2884d250a73e0893d0d9a0093d08dee
yummygyudon <bang2brew@gmail.com>
2022-12-17 14:35:31 +0900

    feat(view$util) : 코치 이름 입력 유효성 검사 기능 구현
    - 2명 이상 입력했는지
    - 2글자 이상 4글자 이하의 이름을 입력했는지

---
+++ src/main/java/menu/view/util/CoachNameInputValidator.java
@@ -0,0 +1,41 @@
+package menu.view.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CoachNameInputValidator {
+        private final String regex = "[0-9a-zA-Zㄱ-ㅎ가-힣]{2,4}";
+        private final String lessCoachErrorMessage = "코치는 최소 2명 이상 입력해야 합니다.";
+        private final String wrongFormatErrorMessage = "올바른 형식으로 입력해주세요";
+        private final int MIN_COACH = 2;
+        private final Pattern pattern = Pattern.compile(regex);
+
+//        public CoachNameInputValidator(){
+//            this.regex = regex;
+//            this.errorMessage = errorMessage;
+//            this.pattern = complie(regex);
+//        }
+
+        public void validate(String ... names){
+            if(names.length < MIN_COACH){
+                throw new IllegalArgumentException(lessCoachErrorMessage);
+            }
+            for(String name : names){
+                Matcher matcher = pattern.matcher(name);
+                if (!matcher.matches()){
+                    throw new IllegalArgumentException(wrongFormatErrorMessage);
+                }
+            }
+        }
+
+    public static void main(String[] args) {
+        CoachNameInputValidator validator = new CoachNameInputValidator();
+        try {
+            validator.validate("가나다","가나다");
+            validator.validate("가나다","가나다","라마바");
+        }catch (IllegalArgumentException ie){
+            System.out.println(ie.getMessage());
+        }
+    }
+
+}
