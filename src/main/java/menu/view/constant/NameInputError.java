d1a5699072c3028774147d5bbf8c14a74da0aeac
yummygyudon <bang2brew@gmail.com>
2022-12-21 11:56:46 +0900

    feat(view$constant) : 이름 입력 관련 에러메세지 클래스 NameInputError 구현
    - public static final 필드를 통해 문자열에 즉시 접근하여 사용할 수 있도록 함.
    - 종류
         1. OVER_LENGTH = 이름의 길이가 4글자를 초과했을 때의 에러메시지("코치 이름은 최대 4글자 이하입니다.")
         2. LACK_LENGTH = 이름의 길이가 2글자 미만일 때의 에러메시지("코치 이름은 최소 2글자 이상입니다.")
         3. OVER_PERSON = 인원 수가 5명을 초과했을 때의 에러메시지("코치 인원은 최대 5명 이하입니다.")
         4. LACK_PERSON = 인원 수가 2명 미만일 때의 에러메시지("코치 인원은 최소 2명 이상입니다.")

---
+++ src/main/java/menu/view/constant/NameInputError.java
@@ -0,0 +1,9 @@
+package menu.view.constant;
+
+public class NameInputError {
+    public static final String OVER_LENGTH = "코치 이름은 최대 4글자 이하입니다.";
+    public static final String LACK_LENGTH = "코치 이름은 최소 2글자 이상입니다.";
+
+    public static final String OVER_PERSON = "코치 인원은 최대 5명 이하입니다.";
+    public static final String LACK_PERSON = "코치 인원은 최소 2명 이상입니다.";
+}
