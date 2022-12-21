b96252d8b778bcb3bc8f3a6994559051af86dcae
yummygyudon <bang2brew@gmail.com>
2022-12-21 11:48:11 +0900

    feat(view$constant) : 결과 출력 시 사용되는 기호 및 구분자 상수 클래스 ResultSign 구현
    - 종류
      1. PREFIX = 접두어("[ ")
      2. SUFFIX = 접미어(" ]"
      3. DELIMITER = 구분자(" | ")

---
+++ src/main/java/menu/view/constant/ResultSign.java
@@ -0,0 +1,7 @@
+package menu.view.constant;
+
+public class ResultSign {
+    public static final String PREFIX = "[ ";
+    public static final String SUFFIX = " ]";
+    public static final String DELIMITER = " | ";
+}
