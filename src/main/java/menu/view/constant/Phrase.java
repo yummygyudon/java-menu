a1b86f9af6160cd2ce45ffd7bb599282c4b98294
yummygyudon <bang2brew@gmail.com>
2022-12-21 11:46:19 +0900

    feat(view$constant) : 시스템 출력 문구 클래스 Phrase 구현
    - public static final 필드를 통해 문자열에 즉시 접근하여 사용할 수 있도록 함.
    - 종류
      1. START = 프로그램 시작문구("점심 메뉴 추천을 시작합니다.")
      2. END = 프로그램 종료문구("점심 메뉴 추천을 시작합니다.")
      3. NOTICE_RESULT = 추천 결과 안내 문구("메뉴 추천 결과입니다.")
      4. REQUEST_NAME = 코치 이름 입력 요청 문구("코치의 이름을 입력해 주세요. (, 로 구분)")
      5. REQUEST_EXCLUDE = 못먹는 음식 입력 요청 문구("(이)가 못 먹는 메뉴를 입력해 주세요.")
        - 이름 부분을 제외한 문구입니다.

---
+++ src/main/java/menu/view/constant/Phrase.java
@@ -0,0 +1,10 @@
+package menu.view.constant;
+
+public class Phrase {
+    public static final String START = "점심 메뉴 추천을 시작합니다.";
+    public static final String END = "추천을 완료했습니다.";
+    public static final String NOTICE_RESULT = "메뉴 추천 결과입니다.";
+
+    public static final String REQUEST_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    public static final String REQUEST_EXCLUDE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+}
