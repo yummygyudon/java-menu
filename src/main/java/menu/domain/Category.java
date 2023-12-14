eab368d0e2325301d80e6df2e5a5900aa84dd865
yummygyudon <bang2brew@gmail.com>
2023-12-14 17:26:47 +0900

    CHORE : Default Domain Setting

---
+++ src/main/java/menu/domain/Category.java
@@ -0,0 +1,30 @@
+package menu.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Category {
+    private final String name ;
+    private final List<String> menus;
+
+
+    public Category(String categoryName, String ... menus){
+        this.name = categoryName;
+        this.menus = new ArrayList<>(Arrays.asList(menus));
+    }
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getMenu() {
+        return Collections.unmodifiableList(menus);
+    }
+
+    public String getRandomMenu(){
+        return Randoms.shuffle(getMenu()).get(0);
+    }
+}
